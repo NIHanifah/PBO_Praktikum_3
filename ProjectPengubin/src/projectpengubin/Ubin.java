cf9ace8bb56eb144b02f1b1f88bfb3351e85290e
NIHanifah <71817419+NIHanifah@users.noreply.github.com>
2021-10-07 18:29:05 +0700

    Add files via upload

---
+++ ProjectPengubin/src/projectpengubin/Ubin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpengubin;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Ubin {
+    int panjang;
+    int lebar;
+    // menghitung luas sebuah ubin
+    int hitungLuas(){
+        int luas = panjang * lebar;
+        return luas;
+    }
+}
